287f5296c5cefa01b672a6d6fd991bf3ca91fd7b
Daulet Baimbetov <daucloudlab@gmail.com>
2015-05-29 12:56:43 +0600

    GPS - пен жұмыс жасау. Қарапайым мысалдар

---
+++ app/src/main/java/kz/abcsoft/gps_demo1/MainActivity.java
@@ -0,0 +1,45 @@
+package kz.abcsoft.gps_demo1;
+
+import android.content.pm.PackageManager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        PackageManager pm = getPackageManager() ;
+
+        Toast.makeText(this, pm.hasSystemFeature(PackageManager.FEATURE_LOCATION_GPS) ? "GPS бар" : "GPS жоқ",
+                Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
